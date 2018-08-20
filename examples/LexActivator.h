bed6c9d5a4abf69df25ca22dabfbdea7df9531b5
Adnan Kamili <adnan.kamili@gmail.com>
2018-08-20 17:52:14 +0530

    added new API function - GetLicenseType()

--- examples/LexActivator.h
+++ examples/LexActivator.h
@@ -311,6 +311,20 @@ LEXACTIVATOR_API int LA_CC GetLicenseUserEmail(STRTYPE email, uint32_t length);
 LEXACTIVATOR_API int LA_CC GetLicenseUserName(STRTYPE name, uint32_t length);
 
 /*
+    FUNCTION: GetLicenseType()
+
+    PURPOSE: Gets the license type (node-locked or hosted-floating).
+
+    PARAMETERS:
+    * licenseType - pointer to a buffer that receives the value of the string
+    * length - size of the buffer pointed to by the name parameter
+
+    RETURN CODES: LA_OK, LA_FAIL, LA_E_PRODUCT_ID, LA_E_TIME, LA_E_TIME_MODIFIED,
+    LA_E_BUFFER_SIZE
+*/
+LEXACTIVATOR_API int LA_CC GetLicenseType(STRTYPE licenseType, uint32_t length);
+
+/*
     FUNCTION: GetActivationMetadata()
 
     PURPOSE: Gets the activation metadata.
