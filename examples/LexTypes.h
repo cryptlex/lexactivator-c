36c2bae48887332eaea479869916fb7b42cb83c0
muneebkq <muneebkq05@gmail.com>
2024-08-22 10:50:54 +0530

    feat: add new type name and stdint header file

--- examples/LexTypes.h
+++ examples/LexTypes.h
@@ -1,7 +1,11 @@
 #ifndef LEX_TYPES_H
 #define LEX_TYPES_H
 
-#define BUFFER_SIZE 256
+#include <stdint.h>
+#define BUFFER_SIZE_256 256
+#define BUFFER_SIZE_2048 2048
+#define BUFFER_SIZE_4096 4096
+#define MAX_METADATA_SIZE 100
 #define MAX_RELEASE_PLATFORMS 10
 #define MAX_LICENSE_KEYS 100
 #define MAX_RELEASE_FILES 10
@@ -17,14 +21,14 @@ typedef struct
     int size;
     int downloads;
     int secured;
-    CHARTYPE id[BUFFER_SIZE];
-    CHARTYPE name[BUFFER_SIZE];
-    CHARTYPE url[BUFFER_SIZE];
-    CHARTYPE extension[BUFFER_SIZE];
-    CHARTYPE checksum[BUFFER_SIZE];
-    CHARTYPE releaseId[BUFFER_SIZE];
-    CHARTYPE createdAt[BUFFER_SIZE];
-    CHARTYPE updatedAt[BUFFER_SIZE];
+    CHARTYPE id[BUFFER_SIZE_256];
+    CHARTYPE name[BUFFER_SIZE_256];
+    CHARTYPE url[BUFFER_SIZE_2048];
+    CHARTYPE extension[BUFFER_SIZE_256];
+    CHARTYPE checksum[BUFFER_SIZE_256];
+    CHARTYPE releaseId[BUFFER_SIZE_256];
+    CHARTYPE createdAt[BUFFER_SIZE_256];
+    CHARTYPE updatedAt[BUFFER_SIZE_256];
 } ReleaseFile;
 
 typedef struct
@@ -32,35 +36,42 @@ typedef struct
     int totalFiles;
     int isPrivate;
     int published;
-    CHARTYPE id[BUFFER_SIZE];
-    CHARTYPE createdAt[BUFFER_SIZE];
-    CHARTYPE updatedAt[BUFFER_SIZE];
-    CHARTYPE name[BUFFER_SIZE];
-    CHARTYPE channel[BUFFER_SIZE];
-    CHARTYPE version[BUFFER_SIZE];
-    CHARTYPE notes[BUFFER_SIZE];
-    CHARTYPE publishedAt[BUFFER_SIZE];
-    CHARTYPE productId[BUFFER_SIZE];
-    CHARTYPE platforms[MAX_RELEASE_PLATFORMS][BUFFER_SIZE];
+    CHARTYPE id[BUFFER_SIZE_256];
+    CHARTYPE createdAt[BUFFER_SIZE_256];
+    CHARTYPE updatedAt[BUFFER_SIZE_256];
+    CHARTYPE name[BUFFER_SIZE_256];
+    CHARTYPE channel[BUFFER_SIZE_256];
+    CHARTYPE version[BUFFER_SIZE_256];
+    CHARTYPE notes[BUFFER_SIZE_4096];
+    CHARTYPE publishedAt[BUFFER_SIZE_256];
+    CHARTYPE productId[BUFFER_SIZE_256];
+    CHARTYPE platforms[MAX_RELEASE_PLATFORMS][BUFFER_SIZE_256];
     ReleaseFile files[MAX_RELEASE_FILES];
 } Release;
 
 typedef struct 
 {
-    CHARTYPE addressLine1[BUFFER_SIZE];
-    CHARTYPE addressLine2[BUFFER_SIZE];
-    CHARTYPE city[BUFFER_SIZE];
-    CHARTYPE state[BUFFER_SIZE];
-    CHARTYPE country[BUFFER_SIZE];
-    CHARTYPE postalCode[BUFFER_SIZE];
+    CHARTYPE addressLine1[BUFFER_SIZE_256];
+    CHARTYPE addressLine2[BUFFER_SIZE_256];
+    CHARTYPE city[BUFFER_SIZE_256];
+    CHARTYPE state[BUFFER_SIZE_256];
+    CHARTYPE country[BUFFER_SIZE_256];
+    CHARTYPE postalCode[BUFFER_SIZE_256];
 } OrganizationAddress;
+
+typedef struct
+{
+    CHARTYPE key[BUFFER_SIZE_256];
+    CHARTYPE value[BUFFER_SIZE_4096];
+} Metadata;
 
 typedef struct
 {
     int64_t allowedActivations;
     int64_t allowedDeactivations;
-    CHARTYPE key[BUFFER_SIZE];
-    CHARTYPE type[BUFFER_SIZE];
+    CHARTYPE key[BUFFER_SIZE_256];
+    CHARTYPE type[BUFFER_SIZE_256];
+    Metadata metadata[MAX_METADATA_SIZE];
 } UserLicense;
 
 #endif // LEX_TYPES_H
