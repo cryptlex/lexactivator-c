28eef356804e1621fbbd6c1e742138d5b8689da5
Huzaif Mushtaq Mir <huzaif@Huzaif-MacBook-Air.local>
2026-07-03 17:03:11 +0530

    chore: add dotnet sample program

---
+++ dotnet/vb-dotnet-45/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.activateBtn = New System.Windows.Forms.Button()
+        Me.activateTrialBtn = New System.Windows.Forms.Button()
+        Me.statusLabel = New System.Windows.Forms.Label()
+        Me.productKeyBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "License Key:"
+        '
+        'activateBtn
+        '
+        Me.activateBtn.Location = New System.Drawing.Point(303, 95)
+        Me.activateBtn.Name = "activateBtn"
+        Me.activateBtn.Size = New System.Drawing.Size(75, 23)
+        Me.activateBtn.TabIndex = 1
+        Me.activateBtn.Text = "Activate"
+        Me.activateBtn.UseVisualStyleBackColor = True
+        '
+        'activateTrialBtn
+        '
+        Me.activateTrialBtn.Location = New System.Drawing.Point(191, 95)
+        Me.activateTrialBtn.Name = "activateTrialBtn"
+        Me.activateTrialBtn.Size = New System.Drawing.Size(92, 23)
+        Me.activateTrialBtn.TabIndex = 2
+        Me.activateTrialBtn.Text = "Activate Trial"
+        Me.activateTrialBtn.UseVisualStyleBackColor = True
+        '
+        'statusLabel
+        '
+        Me.statusLabel.AutoSize = True
+        Me.statusLabel.Location = New System.Drawing.Point(137, 142)
+        Me.statusLabel.Name = "statusLabel"
+        Me.statusLabel.Size = New System.Drawing.Size(40, 13)
+        Me.statusLabel.TabIndex = 3
+        Me.statusLabel.Text = "Status:"
+        '
+        'productKeyBox
+        '
+        Me.productKeyBox.Location = New System.Drawing.Point(140, 37)
+        Me.productKeyBox.Name = "productKeyBox"
+        Me.productKeyBox.Size = New System.Drawing.Size(238, 20)
+        Me.productKeyBox.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(409, 170)
+        Me.Controls.Add(Me.productKeyBox)
+        Me.Controls.Add(Me.statusLabel)
+        Me.Controls.Add(Me.activateTrialBtn)
+        Me.Controls.Add(Me.activateBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Activation Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents activateBtn As Button
+    Friend WithEvents activateTrialBtn As Button
+    Friend WithEvents statusLabel As Label
+    Friend WithEvents productKeyBox As TextBox
+End Class
---
+++ dotnet/vb-dotnet-45/Form1.vb
@@ -0,0 +1,129 @@
+﻿Imports Cryptlex
+
+
+Public Class Form1
+    Public Sub New()
+
+        InitializeComponent()
+
+        Try
+            'LexActivator.SetProductFile ("ABSOLUTE_PATH_OF_PRODUCT.DAT_FILE")
+            LexActivator.SetProductData("PASTE_CONTENT_OF_PRODUCT.DAT_FILE")
+            LexActivator.SetProductId("PASTE_PRODUCT_ID", LexActivator.PermissionFlags.LA_USER)
+            'LexActivator.SetLicenseCallback(AddressOf LicenseCallback)
+            Dim status As Integer = LexActivator.IsLicenseGenuine()
+
+            If status = LexStatusCodes.LA_OK OrElse status = LexStatusCodes.LA_EXPIRED OrElse status = LexStatusCodes.LA_SUSPENDED OrElse status = LexStatusCodes.LA_GRACE_PERIOD_OVER Then
+                Dim expiryDate As UInteger = LexActivator.GetLicenseExpiryDate()
+                Dim daysLeft As Integer = CInt((expiryDate - unixTimestamp())) / 86400
+                Me.statusLabel.Text = "License genuinely activated! Activation Status: " & status.ToString()
+                Me.activateBtn.Text = "Deactivate"
+                Me.activateTrialBtn.Enabled = False
+                Return
+            End If
+
+            status = LexActivator.IsTrialGenuine()
+
+            If status = LexStatusCodes.LA_OK Then
+                Dim trialExpiryDate As UInteger = LexActivator.GetTrialExpiryDate()
+                Dim daysLeft As Integer = CInt((trialExpiryDate - unixTimestamp())) / 86400
+                Me.statusLabel.Text = "Trial period! Days left:" & daysLeft.ToString()
+                Me.activateTrialBtn.Enabled = False
+            ElseIf status = LexStatusCodes.LA_TRIAL_EXPIRED Then
+                Me.statusLabel.Text = "Trial has expired!"
+            Else
+                Me.statusLabel.Text = "Trial has not started or has been tampered: " & status.ToString()
+            End If
+
+            'Checking for software release update
+            'Call SetReleasePlatform() and SetReleaseChannel() before calling CheckForReleaseUpdate()
+            'Release platform and channel must be set before checking for an update
+            'LexActivator.SetReleasePlatform("RELEASE_PLATFORM")
+            'LexActivator.SetReleaseChannel("RELEASE_CHANNEL")
+            'LexActivator.CheckForReleaseUpdate("windows", "1.0.0", "stable", AddressOf SoftwareReleaseUpdateCallback)
+
+        Catch ex As LexActivatorException
+            Me.statusLabel.Text = "Error code: " & ex.Code.ToString() & " Error message: " + ex.Message
+        End Try
+
+    End Sub
+
+    Public Sub activateBtn_Click(sender As Object, e As EventArgs) Handles activateBtn.Click
+        Try
+            Dim status As Integer
+
+            If Me.activateBtn.Text = "Deactivate" Then
+                status = LexActivator.DeactivateLicense()
+
+                If status = LexStatusCodes.LA_OK Then
+                    Me.statusLabel.Text = "License deactivated successfully"
+                    Me.activateBtn.Text = "Activate"
+                    Me.activateTrialBtn.Enabled = True
+                    Return
+                End If
+
+                Me.statusLabel.Text = "Error deactivating license: " & status.ToString()
+                Return
+            End If
+
+            LexActivator.SetLicenseKey(productKeyBox.Text)
+            LexActivator.SetActivationMetadata("key1", "value1")
+            status = LexActivator.ActivateLicense()
+
+            If status = LexStatusCodes.LA_OK OrElse status = LexStatusCodes.LA_EXPIRED OrElse status = LexStatusCodes.LA_SUSPENDED Then
+                Me.statusLabel.Text = "Activation Successful :" & status.ToString()
+                Me.activateBtn.Text = "Deactivate"
+                Me.activateTrialBtn.Enabled = False
+            Else
+                Me.statusLabel.Text = "Error activating the license: " & status.ToString()
+                Return
+            End If
+
+        Catch ex As LexActivatorException
+            Me.statusLabel.Text = "Error code: " & ex.Code.ToString() & " Error message: " + ex.Message
+        End Try
+    End Sub
+
+    Public Sub activateTrialBtn_Click(sender As Object, e As EventArgs) Handles activateTrialBtn.Click
+        Try
+            LexActivator.SetTrialActivationMetadata("key2", "value2")
+            Dim status As Integer = LexActivator.ActivateTrial()
+
+            If status <> LexStatusCodes.LA_OK Then
+                Me.statusLabel.Text = "Error activating the trial: " & status.ToString()
+                Return
+            Else
+                Me.statusLabel.Text = "Trial started Successful"
+            End If
+
+        Catch ex As LexActivatorException
+            Me.statusLabel.Text = "Error code: " & ex.Code.ToString() & " Error message: " + ex.Message
+        End Try
+    End Sub
+
+    Private Sub LicenseCallback(ByVal status As UInteger)
+        ' NOTE: Don't invoke IsLicenseGenuine(), ActivateLicense() or ActivateTrial() API functions in this callback
+        Select Case status
+            Case LexStatusCodes.LA_SUSPENDED
+                Me.statusLabel.Text = "The license has been suspended."
+            Case Else
+                Me.statusLabel.Text = "License status code: " & status.ToString()
+        End Select
+    End Sub
+
+    Private Function unixTimestamp() As UInteger
+        Return CUInt((DateTime.UtcNow.Subtract(New DateTime(1970, 1, 1))).TotalSeconds)
+    End Function
+
+    Private Sub SoftwareReleaseUpdateCallback(ByVal status As UInteger)
+        Select Case status
+            Case LexStatusCodes.LA_RELEASE_UPDATE_AVAILABLE
+                Me.statusLabel.Text = "An update is available for the app."
+            Case LexStatusCodes.LA_RELEASE_NO_UPDATE_AVAILABLE
+                ' Current version is latest
+            Case Else
+                Me.statusLabel.Text = "Release status code: " & status.ToString()
+        End Select
+    End Sub
+
+End Class
