d658a356484ec2a25621ef5a67665a39cc831a24
Huzaif Mushtaq Mir <huzaif@Huzaif-MacBook-Air.local>
2026-08-10 16:16:28 +0530

    chore: ruby nits

--- ruby/sample.rb
+++ ruby/sample.rb
@@ -118,12 +118,5 @@ else
   end
 end
 
-# Checking for software release update
-# Call SetReleaseVersion(), SetReleasePlatform() and SetReleaseChannel() before calling CheckReleaseUpdate()
-# Release version, platform and channel must be set before checking for an update
-# status = LexActivator.SetReleasePlatform(LexActivator::encode_utf16("windows"))
-# status = LexActivator.SetReleaseChannel(LexActivator::encode_utf16("1.0.0"))
-# status = LexActivator.CheckForReleaseUpdate("windows", "1.0.0", "stable", SoftwareReleaseUpdateCallback)
-
 puts "Press Enter to exit..."
 gets
